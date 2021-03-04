6f309677c3cbfce6fed4057e52da0de895481ec3
akashhegdetyson <akash.hegde@tyson.com>
2021-03-04 16:13:28 +0530

    hi

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 arr = []
 arr.append(1)
 arr.append(2)
+<<<<<<< HEAD
 print(arr)
 print(1)
 print(2)
@@ -9,3 +10,6 @@ print('hi')
 hey
 hi
 
+=======
+print(arr)
+>>>>>>> parent of e9e6fd0... changes
